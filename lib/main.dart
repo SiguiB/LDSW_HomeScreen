a719100a2b846f581bb933ab8ce01040956225dc
SiguiB <99350925+SiguiB@users.noreply.github.com>
2023-11-13 17:56:08 -0700

    http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(CatalogoPeliculasApp());
@@ -13,7 +15,30 @@ class CatalogoPeliculasApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final String apiKey = '0828782e9b9a458156058c00318fcc18';
+
+  List<dynamic> movies = [];
+
+  Future<void> getMovies() async {
+    final response = await http.get(
+      Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=$apiKey'),
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        movies = json.decode(response.body)['results'];
+      });
+    } else {
+      throw Exception('Error al obtener datos de películas');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +64,28 @@ class HomeScreen extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 24,
                     color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    await getMovies();
+                    //Actualizar interfaz de usuario con los datos obtenidos aquí
+                  },
+                  child: Text('Obtener Películas Populares'),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: movies.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(movies[index]['title'],
+                        style: TextStyle(color:Colors.white)),
+                        subtitle: Text(movies[index]['overview'],
+                        style: TextStyle(color:Colors.white)),
+                      );
+                    },
                   ),
                 ),
               ],
